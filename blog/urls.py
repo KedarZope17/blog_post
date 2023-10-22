9c5163a158dab4d803ffb6ab4adfaead3858144c
KedarZope17 <zope.kedar97@gmail.com>
2023-10-22 15:28:58 +0530

    urls and views changes in blog app

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+#route over pages
+
 urlpatterns = [
     path('', views.home, name="blog_home"),
     path('about/', views.about, name="blog_about")
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+#create functions to show various pages
 
 def home(request):
     return HttpResponse("Blog Home")
