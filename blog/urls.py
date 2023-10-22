36edc5be84661e990811aa27c0ae26dfb0d66a2e
KedarZope17 <zope.kedar97@gmail.com>
2023-10-22 15:22:40 +0530

    coding standards

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="blog_home"),
+    path('about/', views.about, name="blog_about")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def Home(request):
+def home(request):
     return HttpResponse("Blog Home")
+
+def about(request):
+    return HttpResponse("Information about blog page")
