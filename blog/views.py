5fbacfa47f03732247cc20acd8526772b6808c01
KedarZope17 <zope.kedar97@gmail.com>
2023-11-09 12:37:25 +0530

    New post added

--- blog/views.py
+++ blog/views.py
@@ -18,8 +18,18 @@ posts = [
         'Content' : 'My work of area is little different compared\
             to the other team mates. I am more involved into the backend',
         'Date_posted': 'Nov 01, 2023'
+    },
+        {
+        'Author' : 'Abhishek Tripathi',
+        'Title' : 'Life as an customer relationship manager',
+        'Content' : 'It is crucial to have a good customer support.\
+            I need to analyse the situation, and look for a solution\
+            to team members and handle the client simultaneouly. The\
+            time plays very critical role here and sometimes the pressure\
+            is tough deal.'
     }
 ]
+
 def home(request):
     context = {
         'Posts': posts
