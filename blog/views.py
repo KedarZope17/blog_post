66a743dd057c42f9b71192a428b6068643f5cd3c
KedarZope <zope.kedar97@gmail.com>
2024-01-04 22:51:40 +0530

    Added database handling capabilities for storing posts

--- blog/views.py
+++ blog/views.py
@@ -1,42 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 #create functions to show various pages
-posts = [
-    {
-        'Author' : 'Kedar Zope',
-        'Title' : 'Life as an AI worker specialist',
-        'Content' : 'Work-life balance is little unbalanced,\
-            but the area of work is wide. This enables me to get\
-            into more technologies and understand better\
-            client-handling at the same time.',
-        'Date_posted': 'Nov 01, 2023'
-    },
-    {
-        'Author' : 'Mayuresh K',
-        'Title' : 'Life as an AI worker specialist',
-        'Content' : 'My work of area is little different compared\
-            to the other team mates. I am more involved into the backend',
-        'Date_posted': 'Nov 01, 2023'
-    },
-        {
-        'Author' : 'Abhishek Tripathi',
-        'Title' : 'Life as an customer relationship manager',
-        'Content' : 'It is crucial to have a good customer support.\
-            I need to analyse the situation, and look for a solution\
-           with the respective team members and handle the client simultaneouly. The\
-            time plays very critical role here and sometimes the pressure\
-            is tough deal.',
-        'Date_posted': 'Nov 05, 2023'
-    }
-]
 
 def landing_page(request):
     return render(request, 'blog/landing_page.html', {'Title': 'Welcome'})
 
 def home(request):
     context = {
-        'Posts': posts
+        'Posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
