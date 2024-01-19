f5519e8fd6653acf988c4e22c4bbe8cf259217f5
KedarZope <zope.kedar97@gmail.com>
2024-01-19 19:01:27 +0530

    added post create, update, and delete feature

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.forms.models import BaseModelForm
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import (ListView,
+ DetailView, CreateView, 
+ UpdateView, DeleteView)
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin
+)
+#from django.http import HttpResponse
 from .models import Post
 
 #create functions to show various pages
@@ -12,6 +21,51 @@ def home(request):
         'Posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'Posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    #template_name = 'blog/post_confirm_delete.html'
+    success_url = '/home'
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+        
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
 
 def about(request):
     return render(request, 'blog/about.html', {'Title': 'About'})
